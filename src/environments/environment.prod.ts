accbb7646f64e23b2beaa9588e41260ad28f17d2
lethieuanh89 <leanh1541989@gmail.com>
2026-02-26 17:55:27 +0700

    fix

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBYobzz5lb5N6T-C-ChsOEA6SQuhexa0e8",
+    authDomain: "hodoxuanthuong.firebaseapp.com",
+    projectId: "hodoxuanthuong",
+    storageBucket: "hodoxuanthuong.firebasestorage.app",
+    messagingSenderId: "200363966715",
+    appId: "1:200363966715:web:c26e7f0294af948b4db05e",
+    measurementId: "G-170G1FWEMX"
+  },
+};
